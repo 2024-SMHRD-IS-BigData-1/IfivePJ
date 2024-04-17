01647c0661c301183dfb2b4c4a100c2084bca9e2
SMHRD <SMHRD@211.228.36.192>
2024-04-17 16:44:40 +0900

    0417/16:44

---
+++ ifiveproject/src/main/webapp/sql.sql
@@ -0,0 +1,6 @@
+select * from chatting_info;
+select * from user_info;
+INSERT INTO chatroom_info VALUES ('user_id 2', 'room_title 2', 'room_info 2', 1, sysdate, 'room_status 2');
+INSERT INTO user_info VALUES ('user_id 2', 'user_pw 2', 1, 1, 1, sysdate, 'user_type2');
+    
+INSERT INTO chatroom_info VALUES('user_id 1', 'room_title 1', 'room_info 1', 1, sysdate, 'room_status 1');
