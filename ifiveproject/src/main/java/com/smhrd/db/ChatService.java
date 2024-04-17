254a4bacd9bbd1071c650066b75afdd7f267c2eb
SMHRD <SMHRD@211.228.36.192>
2024-04-17 17:55:25 +0900

    aa

--- ifiveproject/src/main/java/com/smhrd/db/ChatService.java
+++ ifiveproject/src/main/java/com/smhrd/db/ChatService.java
@@ -3,11 +3,21 @@ package com.smhrd.db;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.smhrd.model.chatting_info;
+
 public class ChatService {
 	
 	public String execute(HttpServletRequest request, HttpServletResponse response) {
+		String chat_idx = request.getParameter("chat_idx");
+		String room_idx = request.getParameter("room_idx");
+		String chatter= request.getParameter("chatter");
+		String chating = request.getParameter("chating");
+		String emoticon= request.getParameter("emoticon");
+		String chatted_at = request.getParameter("chatted_at");
 		
-		// 메세지 보내기
+		chatting_info sendChat = new Chat(chat_idx,room_idx,chatter,chating,emoticon,chatted_at);
+		
+		
 		return null;
 	}
 }
