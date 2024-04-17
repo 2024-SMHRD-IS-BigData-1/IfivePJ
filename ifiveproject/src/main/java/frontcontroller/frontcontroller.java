fc3d10a5fc0d1c5679d63619ac4c72cbbc8fa558
smhrd <smhrd@210.223.239.237>
2024-04-17 15:40:31 +0900

    ㅁ

--- ifiveproject/src/main/java/frontcontroller/frontcontroller.java
+++ ifiveproject/src/main/java/frontcontroller/frontcontroller.java
@@ -7,11 +7,37 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.smhrd.frontcontroller.Command;
+
 
 @WebServlet("*.do")
 public class frontcontroller extends HttpServlet {
+	public interface Command {
+
+	}
+
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
+		
+		String requestURI =  request.getRequestURI();
+		
+		String contextpath =  request.getContextPath();
+		
+		String resultURL = requestURI.substring(contextpath.length() + 1);
+		
+		Command service = null;
+		
+		
+		
+//		if (resultURL.equals("LoginService.do")) {
+//			 service = new LoginService();
+//		}
+		
+		String moveURL =service.execute(request, response);
+		
+		if (moveURL != null) {
+		response.sendRedirect(moveURL);
+		}
 	
 	
 	}
